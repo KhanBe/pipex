e32f025cd59b7034ce1fd8ab4b89aefa1f06e1cc
KhanBe <wistm6980@gmail.com>
2022-06-19 22:58:26 +0900

    header file

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,8 +6,33 @@
 /*   By: jaewoo <jaewoo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/25 16:55:25 by jaewoo            #+#    #+#             */
-/*   Updated: 2022/05/25 16:55:27 by jaewoo           ###   ########.fr       */
+/*   Updated: 2022/06/16 23:34:28 by jaewoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/pipex.h"
 
+int	main(int argc, char **argv, char **envp)
+{
+	pid_t	pid1;
+	int		fd[2];
+
+	if (argc == 5)
+	{
+		if (pipe(fd) == -1)
+			error();
+		pid1 = fork();
+		if (pid1 == -1)
+			error();
+		if (pid1 == 0)
+			child_process(argv, envp, fd);
+		waitepid(pid1, NULL, 0);
+		parent_process(argv, envp, fd);
+	}
+	else
+	{
+		ft_putstr_fd("arguments error\n", 2);
+		ft_putstr_fd("ex) ./pipex file1 cmd1 cmd2 file2\n", 1);
+	}
+	return (0);
+}
