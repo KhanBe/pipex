45637e8743b6d89d269271f8ff4951766aef0729
KhanBe <wistm6980@gmail.com>
2022-06-19 22:57:43 +0900

    header file

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,8 +6,28 @@
 /*   By: jaewoo <jaewoo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/25 16:54:50 by jaewoo            #+#    #+#             */
-/*   Updated: 2022/05/25 16:54:52 by jaewoo           ###   ########.fr       */
+/*   Updated: 2022/06/19 22:57:18 by jaewoo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PIPEX_H
+# define PIPEX_H
 
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include "../libft/libft.h"
+
+void	error(void);
+char	*find_path(char *cmd, char **envp);
+void	execute(char *argv, char **envp);
+int		get_next_line(char **line);
+
+int		open_file(char *argv, int i);
+void	usage(void);
+
+
+endif
